bc7bd27f3812d17e6ac5ea315c71adfeb1678e1f
vova_lovalova <rudchenko.vova@mail.ru>
2023-03-08 00:14:05 +0800

    оптимизация
    перфоманс 78%

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
+// import {iosVhFix} from './utils/ios-vh-fix';
+// import {initModals} from './modules/modals/init-modals';
+// import {Form} from './modules/form-validate/form';
 
 // ---------------------------------
 
@@ -176,19 +176,19 @@ window.addEventListener('DOMContentLoaded', () => {
   // Utils
   // ---------------------------------
 
-  iosVhFix();
+  // iosVhFix();
 
-  // Modules
-  // ---------------------------------
+  // // Modules
+  // // ---------------------------------
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
-  });
+  // // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // // в load следует добавить скрипты, не участвующие в работе первого экрана
+  // window.addEventListener('load', () => {
+  //   initModals();
+  //   const form = new Form();
+  //   window.form = form;
+  //   form.init();
+  // });
 });
 
 // ---------------------------------
